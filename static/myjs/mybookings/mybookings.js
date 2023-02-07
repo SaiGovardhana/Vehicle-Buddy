e47ece4d49aa3bcdad8b16b402df65de29163195
SaiGovardhana <191fa04292@gmail.com>
2023-02-07 21:19:55 +0530

    Added The Bookings page and fixed card look

--- static/myjs/mybookings/mybookings.js
+++ static/myjs/mybookings/mybookings.js
@@ -5,14 +5,22 @@ async function doAjax()
 }
 
 async function doRender(data)
-{   let dataFormat={
+{   if(data.data.length==0)
+    {    $("#table").remove();
+        $("#servicecontainer").html("<h2 class='h2 text-center'>User doesn't have any vehicles.</h2>")
+        return ;
+    }
+    let dataFormat={
     data:data.data,
     columns:
     [
-        {title:"Vehicle Name",data:"vehicleid"},
-        {title:"Vehicle Price",data:"vehicleprice"}
-    ]
-        };
+        {title:"Vehicle Name",data:"vehiclename"},
+        {title:"Vehicle Price",data:"vehicleprice"},
+        {title:"Vehicle Location",data:"vehiclelocation"},
+        {title:"Booking date",data:"bookingdate", render:(data)=>(new Date(data)).toLocaleDateString()},
+        {title:"Seller Email",data:"selleremail"}
+    ],
+    "columnDefs": [{ className: "dt-head-center", targets: [ 0,1,2,3,4 ] }]};
 
     $("#table").DataTable(dataFormat);
 }
